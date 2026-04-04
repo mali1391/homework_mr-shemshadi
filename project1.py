adca05735b4b887db989d566d26514312e31da2d
mohammadali <malimotaharifar@chmail.ir>
2026-04-04 14:59:38 +0330

    fix(message for merge sort)

--- project1.py
+++ project1.py
@@ -1,13 +1,26 @@
-def mergeSort(arr):
+def mergeSort(arr : list, message : str):
   if len(arr) <= 1:
     return arr
-  mid = len(arr) // 2
-  leftHalf = arr[:mid]
-  rightHalf = arr[mid:]
-  sortedLeft = mergeSort(leftHalf)
-  sortedRight = mergeSort(rightHalf)
-  return merge(sortedLeft, sortedRight)
 
+  if message == "up":
+    mid = len(arr) // 2
+    leftHalf = arr[:mid]
+    rightHalf = arr[mid:]
+    sortedLeft = mergeSort(leftHalf, message)
+    sortedRight = mergeSort(rightHalf, message)
+    return merge(sortedLeft, sortedRight)
+
+  elif message == "down":
+    mid = len(arr) // 2
+    leftHalf = arr[:mid]
+    rightHalf = arr[mid:]
+    sortedLeft = mergeSort(leftHalf, message)
+    sortedRight = mergeSort(rightHalf, message)
+    return merge2(sortedLeft, sortedRight)
+
+  else:
+    print("error")
+    return False
 def merge(left, right):
   result = []
   i = 0
@@ -23,6 +36,22 @@ def merge(left, right):
   result.extend(right[j:])
   return result
 
+def merge2(left, right):
+  result2 = []
+  i = 0
+  j = 0
+  while i < len(left) and j < len(right):
+    if left[i] > right[j]:
+      result2.append(left[i])
+      i += 1
+    else:
+      result2.append(right[j])
+      j += 1
+  result2.extend(left[i:])
+  result2.extend(right[j:])
+  return result2
+
 my_list = [3, 7, 6, -10, 15, 23.5, 55, -13]
-my_sortedlist = mergeSort(my_list)
+txt_message = input("Enter your message: ")
+my_sortedlist = mergeSort(my_list, txt_message)
 print("Sorted array:", my_sortedlist)
