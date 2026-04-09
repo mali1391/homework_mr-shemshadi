3c2cd147d5c82f03e22ba2402735cedba98dbd7f
mohammadali <malimotaharifar@chmail.ir>
2026-04-09 20:06:54 +0330

    feat: add a untrackt file.

---
+++ func_exerscise.py
@@ -0,0 +1,11 @@
+list1 = [15685, 986, 7471, 8856]
+for item in list1:
+    odd = lambda x: item % 2
+
+    my_list = list(map(odd, list1))
+    if item:
+        list1.remove(item)
+    else:
+        pass
+filter_list = lambda filter: filter(list1)
+print(list1)
