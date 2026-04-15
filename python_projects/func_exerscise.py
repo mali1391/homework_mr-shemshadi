3de7fe0c98fb3772186d609a23b6175320ce6c73
mohammadali <malimotaharifar@chmail.ir>
2026-04-15 13:50:37 +0330

    perf: remove the surplusage lines and rename variable name.

--- python_projects/func_exerscise.py
+++ python_projects/func_exerscise.py
@@ -1,11 +1,9 @@
 list1 = [15685, 986, 7471, 8856]
 for item in list1:
-    odd = lambda x: item % 2
+    odd_number = lambda x: item % 2
 
-    my_list = list(map(odd, list1))
+    my_list = list(map(odd_number, list1))
     if item:
         list1.remove(item)
-    else:
-        pass
-filter_list = lambda filter: filter(list1)
+filter_list = lambda filterer: filter(list1)
 print(list1)
