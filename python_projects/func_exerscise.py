936dbb42f87b65af62bbb5ae0ea7a149110b75fb
mohammadali <malimotaharifar@chmail.ir>
2026-04-15 18:16:19 +0330

    refactor: rename the variable name.

--- python_projects/func_exerscise.py
+++ python_projects/func_exerscise.py
@@ -1,9 +1,9 @@
-list1 = [15685, 986, 7471, 8856]
-for item in list1:
+list_of_numbers = [15685, 986, 7471, 8856]
+for item in list_of_numbers:
     odd_number = lambda x: item % 2
 
-    my_list = list(map(odd_number, list1))
+    my_list = list(map(odd_number, list_of_numbers))
     if item:
-        list1.remove(item)
-filter_list = lambda filterer: filter(list1)
-print(list1)
+        list_of_numbers.remove(item)
+filter_list = lambda filterer: filter(list_of_numbers)
+print(list_of_numbers)
