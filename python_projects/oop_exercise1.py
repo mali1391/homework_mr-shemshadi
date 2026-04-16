e93e1a589585068119c42885f86f19bf5585bdb1
mohammadali <malimotaharifar@chmail.ir>
2026-04-16 17:38:54 +0330

    feat: add homework number1

--- python_projects/oop_exercise1.py
+++ python_projects/oop_exercise1.py
@@ -1,10 +1,25 @@
 class Formula:
-    def __init__(self, Number_of_parameters, Mathematical_relationships_between_the_parameters, Formula_name, Type_of_output):
+    def __init__(self, number_of_parameters, mathematical_relationships_between_the_parameters, formula_name, type_of_output):
 
-        self.number_of_parameters = Number_of_parameters
-        self.mathematical_relationships_between_the_parameters = Mathematical_relationships_between_the_parameters
-        self.Formula_name = Formula_name
-        self.Type_of_output = Type_of_output
+        self.number_of_parameters = number_of_parameters
+        self.mathematical_relationships_between_the_parameters = mathematical_relationships_between_the_parameters
+        self.Formula_name = formula_name
+        self.Type_of_output = type_of_output
 
-    def parameters(self):
-        if self.number_of_parameters:
+class Math_formula(Formula):
+    def __init__(self, number_of_parameters, mathematical_relationships_between_the_parameters, formula_name, type_of_output, triangular_pattern):
+        Formula.__init__(number_of_parameters, mathematical_relationships_between_the_parameters, formula_name, type_of_output)
+        self.triangular_pattern = triangular_pattern
+
+    def triangular_pattern(self):
+        if self.number_of_parameters > 0:
+
+
+
+# number = int(input("Enter the number of parameters: "))
+#
+# formula_name = input("Enter the formula name: ")
+#
+# formula = Formula(number, 5, 4, 7 )
+#
+# Formula.parameters()
