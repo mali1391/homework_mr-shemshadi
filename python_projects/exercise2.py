c5ffb8b5e2431ae9baf4f2ec0de3a0d12df3a2d5
mohammadali <malimotaharifar@chmail.ir>
2026-05-30 23:13:28 +0330

    feat: add exercise 2

---
+++ python_projects/exercise2.py
@@ -0,0 +1,17 @@
+def check_word(word1 : str, word2 : str):
+    len_word1 = len(word1)
+    len_word2 = len(word2)
+
+    if len_word1 > len_word2:
+        return word1
+    elif len_word1 < len_word2:
+        return word2
+    elif len_word1 == len_word2:
+        print("these words are equal")
+
+name1 = input("enter first word: ")
+name2 = input("enter second word: ")
+
+result = check_word(name1, name2)
+
+print(result)
