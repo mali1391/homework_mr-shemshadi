f7ea3f6e82ee6a11fce747d2cbada3a6baac392b
mohammadali <malimotaharifar@chmail.ir>
2026-06-19 18:45:40 +0330

    fix: handle set in this exercise

--- python_projects/exercise5.py
+++ python_projects/exercise5.py
@@ -1,23 +1,15 @@
 def check_list(list1: list, set1: set):
-    for x in range(len(set1)):
+        for x in set1:
+            pass
         for i in range(len(list1)):
-            if list1[i] in set1:
-                if list1[i] == set1:
-                    list1.remove(list1[i])
-                    del (set1)
-                elif list1[i] != set1:
-                    list1.remove(list1[i])
-                return list1, set1
-            if set1 in list1:
-                if set1 == list1[i]:
-                    list1.remove(list1[i])
-                    del (set1)
-                elif set1[x] != list1[i]:
-                    del (set1)
-                return list1, set1
+            if list1[i] in set1 and x in list1:
+                list1.remove(list1[i])
+                set1.remove(x)
+                set1.update(list1)
+        return set1
 
 list1 = [52, 84, 15]
-set1 = {15, 32, 85}
+set1 = {15, 32, 44}
 result = check_list(list1, set1)
 
 print(result)
